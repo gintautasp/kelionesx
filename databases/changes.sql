6c38acff0317082cd72440133b403da38068af9a
Edvinas.Stepanenko <edvinas.stepanenko@gmail.com>
2021-01-18 14:14:46 +0200

    int>varchar aprasymas

--- databases/changes.sql
+++ databases/changes.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `keliones`  ADD `data` VARCHAR(255) NOT NULL  AFTER `pav`,  ADD `laikas` VARCHAR(255) NOT NULL  AFTER `data`,  ADD `trukme` VARCHAR(255) NOT NULL  AFTER `laikas`;
+
+ALTER TABLE `punktai` CHANGE `aprasymas` `aprasymas` VARCHAR(255) NOT NULL; 
