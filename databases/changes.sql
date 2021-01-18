af52377dcc01e7906ae810457b346add06e4c43b
Edvinas.Stepanenko <edvinas.stepanenko@gmail.com>
2021-01-18 14:33:36 +0200

    int>text aprasymas

--- databases/changes.sql
+++ databases/changes.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `keliones`  ADD `data` VARCHAR(255) NOT NULL  AFTER `pav`,  ADD `laikas` VARCHAR(255) NOT NULL  AFTER `data`,  ADD `trukme` VARCHAR(255) NOT NULL  AFTER `laikas`;
 
-ALTER TABLE `punktai` CHANGE `aprasymas` `aprasymas` VARCHAR(255) NOT NULL; 
+ALTER TABLE `lankymasis_punkte` CHANGE `aprasymas` `aprasymas` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL; 
